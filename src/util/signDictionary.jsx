880f312f659248a4eece2e704760a0b369ed97df
KentTDang <kent_2015@outlook.com>
2024-10-13 00:12:22 -0400

    lol

---
+++ src/util/signDictionary.jsx
@@ -0,0 +1,13 @@
+export const signDictionary = {
+    "0": "path/to/0.mp4",
+    "1": "path/to/1.mp4",
+    "2": "path/to/2.mp4",
+    "3": "path/to/3.mp4",
+    "4": "path/to/4.mp4",
+    "5": "path/to/5.mp4",
+    "6": "path/to/6.mp4",
+    "7": "path/to/7.mp4",
+    "8": "path/to/8.mp4",
+    "9": "path/to/9.mp4",
+    "A": "path/to/A.mp4",
+};
