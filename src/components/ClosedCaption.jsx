c42a88b4dae492a2c7c24375f69cdfc342334abf
KentTDang <kent_2015@outlook.com>
2024-10-13 01:02:36 -0400

    the videos are working

--- src/components/ClosedCaption.jsx
+++ src/components/ClosedCaption.jsx
@@ -1,11 +1,16 @@
 import React, { useEffect, useState, useCallback } from "react";
+import { signDictionary } from "../util/signDictionary";
 
 export default function ClosedCaption() {
   const [caption, setCaption] = useState("");
+  const [aslTranslation, setAslTranslation] = useState([]);
 
   const handleMessage = useCallback((request) => {
-    if(request.action === "captionUpdate") {
+    if (request.action === "captionUpdate") {
+      console.log("Closed Caption Request Test: ", request.text);
       setCaption(request.text);
+      setAslTranslation(translateToASL(request.text));
+      console.log("Hoping this works:", aslTranslation);
     }
   }, []);
 
@@ -14,10 +19,42 @@ export default function ClosedCaption() {
     return () => chrome.runtime.onMessage.removeListener(handleMessage);
   }, [handleMessage]);
 
+  function translateToASL(text) {
+    const words = text.toLowerCase().split(/\s+/);
+    return words.map((word) => {
+      if (word in signDictionary) {
+        return { 
+          word: word, 
+          asset: chrome.runtime.getURL(signDictionary[word])
+        };
+      }
+      return { word: word, asset: null };
+    });
+  }
+
   return (
     <>
-    <p>Hello World</p>
-    <p>{caption}</p>
+      <>
+        <p>Caption: {caption}</p>
+        <div>
+          ASL Translation:
+          {aslTranslation.map((item, index) => (
+            <span key={index}>
+              {item.asset ? (
+                <video
+                  src={item.asset}
+                  alt={item.word}
+                  style={{ width: "50px", height: "50px" }}
+                  autoPlay
+                  loop
+                />
+              ) : (
+                item.word + " "
+              )}
+            </span>
+          ))}
+        </div>
+      </>
     </>
-  )
+  );
 }
